5a0067cc49076c28c30d931756e755aba8786ba0
vinzucodes <iamvinzu@gmail.com>
2025-06-30 17:28:09 +0800

    adjust nav

--- src/components/molecules/TopNavigationBar.tsx
+++ src/components/molecules/TopNavigationBar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const TopNavigationBar = () => {
   return (
-    <div className='w-full p-5 px-10 flex justify-center items-center'>
+    <div className='w-full pt-10 px-10 flex justify-center items-center'>
         <div className='flex'>
             <Image src="/main-logo-2.png" alt="Coffee Shop Image" width={200} height={100} />
         </div>
